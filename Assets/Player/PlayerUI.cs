130716041ad7bd89218e81138ce278c3b9a97aad
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-05-20 15:22:31 +0500

    Обводка текущего персонажа

--- Assets/Player/PlayerUI.cs
+++ Assets/Player/PlayerUI.cs
@@ -14,6 +14,7 @@ namespace Player
 		[SerializeField]private HealthBar healthBar;
 		[SerializeField]private Canvas openCharactersCanvas;
 		[SerializeField]private List<Image> openCharactersImages = new List<Image>();
+		[SerializeField] private Material OutlineMaterial;
 
 		private void UpdateAmmoText(object sender, System.EventArgs eventArgs)
 		{
@@ -28,28 +29,45 @@ namespace Player
 
 		private void UpdateCharacters(object sender, System.EventArgs eventArgs)
 		{
-			foreach (var image in openCharactersImages)
-			{
+			foreach (var image in openCharactersImages) 
 				Destroy(image.gameObject);
-			}
 			openCharactersImages.Clear();
-			
+
 			var yNextPosition = 0f;
 			foreach (var character in player.unlockedCharacters)
 			{
-				var characterImage = new GameObject("CharacterImage");
+				if (character == player.character)
+				{
+					var outlineImageObj = new GameObject("CharacterImage");
+					
+					var rectTransform1 = outlineImageObj.AddComponent<RectTransform>();
+					rectTransform1.transform.SetParent(openCharactersCanvas.transform);
+					rectTransform1.localScale = Vector3.one;
+					rectTransform1.anchoredPosition = new Vector2(0, yNextPosition);
+					rectTransform1.sizeDelta= new Vector2(110, 110);
+					
+					var outlineImage = outlineImageObj.AddComponent<Image>();
+					outlineImage.sprite = character.sprite.sprite;
+					outlineImage.material = OutlineMaterial;
+					outlineImage.transform.SetParent(openCharactersCanvas.transform);
 
-				var rectTransform = characterImage.AddComponent<RectTransform>();
+					openCharactersImages.Add(outlineImage);
+				}
+				
+				
+				var characterImageObj = new GameObject("CharacterImage");
+
+				var rectTransform = characterImageObj.AddComponent<RectTransform>();
 				rectTransform.transform.SetParent(openCharactersCanvas.transform);
 				rectTransform.localScale = Vector3.one;
 				rectTransform.anchoredPosition = new Vector2(0, yNextPosition);
 				rectTransform.sizeDelta= new Vector2(100, 100);
 				yNextPosition -= rectTransform.rect.height;
 
-				var image = characterImage.AddComponent<Image>();
+				var image = characterImageObj.AddComponent<Image>();
 				image.sprite = character.sprite.sprite;
-				characterImage.transform.SetParent(openCharactersCanvas.transform);
-				
+				characterImageObj.transform.SetParent(openCharactersCanvas.transform);
+
 				openCharactersImages.Add(image);
 			}
 		}
