e41081e04ad676114061f04f40c8c8bc4733feca
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-05-28 20:51:12 +0500

    Код для патронов для чела с файерболлами и бомбами

---
+++ Assets/Loot/FireballAmmoPickUp.cs
@@ -0,0 +1,22 @@
+﻿using Player;
+using UnityEngine;
+using Weapon;
+
+namespace Bullet
+{
+	public class FireballAmmoPickUp : MonoBehaviour
+	{
+		private int bulletsAmount = 10;
+		public ParticleSystem SpawnAnimation;
+
+		private void Start()
+		{
+			Instantiate(SpawnAnimation, transform.position + new Vector3(0, 0, -2), Quaternion.identity);
+		}
+		public void OnTriggerEnter2D(Collider2D other)
+		{
+			var ammoPickup = new AmmoPickUp<FireballLauncher>();
+			ammoPickup.PickUp(gameObject, other, bulletsAmount);
+		}
+	}
+}
---
+++ Assets/Loot/GrenadeAmmoPickUp.cs
@@ -0,0 +1,22 @@
+﻿using Player;
+using UnityEngine;
+using Weapon;
+
+namespace Bullet
+{
+	public class GrenadeAmmoPickUp : MonoBehaviour
+	{
+		private int bulletsAmount = 10;
+		public ParticleSystem SpawnAnimation;
+
+		private void Start()
+		{
+			Instantiate(SpawnAnimation, transform.position + new Vector3(0, 0, -2), Quaternion.identity);
+		}
+		public void OnTriggerEnter2D(Collider2D other)
+		{
+			var ammoPickup = new AmmoPickUp<GrenadeLauncher>();
+			ammoPickup.PickUp(gameObject, other, bulletsAmount);
+		}
+	}
+}
