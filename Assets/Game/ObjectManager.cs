29354c9a9614295acccbab123b1da84d4b3d40a2
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2022-05-31 18:54:56 +0500

    Выключение света за камерой

---
+++ Assets/Game/ObjectManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+	public class ObjectManager : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (!other.transform.CompareTag("Respawn"))
+				return;
+			for (var i = 0; i < other.transform.childCount; i++) 
+				other.transform.GetChild(i).gameObject.SetActive(true);
+		}
+
+		private void OnTriggerExit2D(Collider2D other)
+		{
+			if (!other.transform.CompareTag("Respawn"))
+				return;
+			for (var i = 0; i < other.transform.childCount; i++)
+				other.transform.GetChild(i).gameObject.SetActive(false);
+		}
+	}
+}
