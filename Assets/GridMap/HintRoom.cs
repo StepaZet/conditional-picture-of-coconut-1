651a3ced1c5757480f06d39a678626750912ba4d
StepaZet <stepa.zet2@gmail.com>
2022-06-18 19:28:58 +0500

    Обучение, на ней траблы с UI

---
+++ Assets/GridMap/HintRoom.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintRoom : MonoBehaviour
+{
+    public GameObject help;
+    public GameObject hint;
+    public GameObject hintBack;
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.gameObject.layer != 9)
+            return;
+        if (help != null)
+            help.gameObject.SetActive(true);
+        hint.gameObject.SetActive(true);
+        hintBack.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D obj)
+    {
+        if (obj.gameObject.layer != 9)
+            return;
+        hint.gameObject.SetActive(false);
+        hintBack.gameObject.SetActive(false);
+    }
+}
